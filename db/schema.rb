41d6716d1e4a77fe6893e23f620b08fafd2ed5ba
tanhongit <phuongtan12357nguyen@gmail.com>
2020-08-10 02:39:09 +0700

    change sqlite to postgresql because to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_07_16_034652) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "resumes", force: :cascade do |t|
     t.string "name"
     t.string "attachment"
